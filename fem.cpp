ceb4a78217f44a078d9f1fd10408b8a783532179
guihunkun <615815574@qq.com>
2021-12-19 16:53:37 +0800

    FEM

--- fem.cpp
+++ fem.cpp
@@ -7,6 +7,128 @@
 
 #include "fem.h" 
 
+
+bool Polygon::setPoint(const unsigned int index, const Point& p)
+{
+    if(index >= _vertex.size()) {
+      return false;
+    }
+    _vertex[index] = p;
+    return true;
+}
+
+bool Polygon::setPoint(const unsigned int index, 
+                   const double x, const double y, const double z)
+{
+    if(index >= _vertex.size()) {
+      return false;
+    }
+    _vertex[index].setX(x);
+    _vertex[index].setY(y);
+    _vertex[index].setZ(z);
+    return true;
+}
+
+
+const double Polygon::getArea() const
+{
+    int n = _vertex.size();
+    if(n < 3) {
+      return 0.0f;
+    }
+    double area = 0.0f;
+    double x,y;
+    y = _vertex[0].getY();
+    x = _vertex[n-1].getX();
+    area = y*(x - _vertex[1].getX());
+    for(int i = 1; i < n; ++i)
+    {
+      y = _vertex[i].getY();
+      x = _vertex[i-1].getX();
+      area += y*(x - _vertex[(i+1)%n].getX());
+    }
+    return fabs(area*0.5);
+}
+
+bool Polygon::getXYRange(double& minx, double& maxx, 
+                     double& miny, double& maxy) const
+{
+    if(!_vertex.empty())
+    {
+      minx = _vertex[0].getX();
+      maxx = _vertex[0].getX();
+      miny = _vertex[0].getY();
+      maxy = _vertex[0].getY();
+      for(unsigned int i = 1; i < _vertex.size(); ++i)
+      {
+        minx = min(minx, _vertex[i].getX());
+        maxx = max(maxx, _vertex[i].getX());
+        miny = min(miny, _vertex[i].getY());
+        maxy = max(maxy, _vertex[i].getY());
+      }
+      return true;
+    } else {
+      return false;
+    }
+}
+
+bool Polygon::getCenter(Point& p) const
+{
+    double minx, maxx, miny, maxy;
+    getXYRange(minx, maxx, miny, maxy);
+    p.setX((minx+maxx)/2.0);
+    p.setY((miny+maxy)/2.0);
+    return true;
+}
+
+double Polygon::getDistance(const Point& p) const
+{
+    double minx, maxx, miny, maxy;
+    getXYRange(minx, maxx, miny, maxy);
+    double pox = (minx+maxx)/2.0;
+    double poy = (miny+maxy)/2.0;
+    double dis = (pox-p.getX())*(pox-p.getX()) + (poy-p.getY())*(poy-p.getY());
+    if(Equal(dis, 0.0)) {
+      return 0.0;
+    }
+    return sqrt(dis);
+}
+  
+  
+bool Polygon::isRectangle() const
+{
+    for(unsigned int i = 0; i < _vertex.size(); ++i)
+    {
+      Point p1 = _vertex[i];
+      Point p2 = _vertex[i == _vertex.size()-1 ? 0 : i+1];
+      if(!Equal(p1.getY(), p2.getY()))
+      {
+        if(!Equal(p1.getX(), p2.getX())) {
+          return false;
+        }
+      }
+    }
+    return true;
+}
+
+
+bool Polygon::isInPolygon(const Point& p) const
+{
+    double minx, maxx, miny, maxy;
+    getXYRange(minx, maxx, miny, maxy);
+    double x = p.getX();
+    double y = p.getY();
+    if(Less(minx, x) && Less(x, maxx) && Less(miny, y) && Less(y, maxy)) {
+      return true;
+    }
+    return false;
+}
+
+
+  
+  
+  
+
 using namespace std;
 int main()
 {
--- fem.h
+++ fem.h
@@ -2,6 +2,24 @@
 #define __FEM__
 
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <math.h>
+
+
+using namespace std;
+
+const double FLOAT_EPS = 1e-10;
+template<class T1, class T2>  static bool Equal(T1 t1, T2 t2) {return fabs(t1-t2) < FLOAT_EPS;}
+template<class T1, class T2>  static bool Greater(T1 t1, T2 t2) {return t1-t2 > FLOAT_EPS;}
+template<class T1, class T2>  static bool Less(T1 t1, T2 t2) {return t2-t1 > FLOAT_EPS;}
+template<class T1, class T2>  static bool GreaterOrEqual(T1 t1, T2 t2) {return fabs(t1-t2) >= FLOAT_EPS;}
+template<class T1, class T2>  static bool LessOrEqual(T1 t1, T2 t2) {return t1-t2 <= FLOAT_EPS;}
+
+
 class Point
 {
 /**
@@ -22,6 +40,30 @@ class Point
     double _x, _y, _z;
 };
 
+class Polygon
+{
+	public:
+	  Polygon() : _name(""), _vertex() { }
+	  Polygon(const string& name, const vector<Point>& vertex) : _name(name), _vertex(vertex) {}
+	  ~Polygon();
+	  const vector<Point>& getVertex() const {return _vertex;}
+      void clear() {_vertex.clear();}
+      void append(const Point& p) {_vertex.push_back(p);}
+      bool setPoint(const unsigned int index, const Point& p);
+      bool setPoint(const unsigned int index, const double x, const double y, const double z);
+      void setPoints(const vector<Point>& vertex) {_vertex.clear();_vertex = vertex;}
+      const double getArea() const;
+      bool getXYRange(double& minx, double& maxx, double& miny, double& maxy) const;
+      bool getCenter(Point& p) const;
+      // calculate distance of p and poly center
+      double getDistance(const Point& p) const; 
+      bool isRectangle() const;
+      bool isInPolygon(const Point& p) const;		
+	
+	private:
+		string _name;
+		vector<Point> _vertex;
+};
 
 
 
